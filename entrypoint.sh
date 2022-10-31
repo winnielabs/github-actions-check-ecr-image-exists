3df5641b4ceaea0b0ea56062b4507dccfdd2db3f
James Pearson Hughes <james@winnie.com>
2022-10-31 15:10:31 -0700

    cd into app directory
    This shouldn't be necessary since it's our working directory already,
    but it seems it is.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 set -eu
 
+cd /usr/src/app
 image_exists=$(go run ecr.go -region="$1" -repository-name="$2" -tag="$3" -access-key-id="$4" -secret-access-key="$5")
 echo "::set-output name=image-exists::$image_exists"
