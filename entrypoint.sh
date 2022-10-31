97be04194500224f316ff5f59f81fb38407410d3
James Pearson Hughes <james@winnie.com>
2022-10-31 14:01:51 -0700

    Clean up shell script
    There were a couple latent problems with spaces in the inputs, but most
    importantly if the go script failed, this script would not fail, and
    thus that failure wouldn't be passed back through to Actions.
    
    We should probably remove the shell script entirely and just have the go
    script output things in the right format, but this is the first easy
    change.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
+set -eu
 
-image_exists=$(go run ecr.go -region=$1 -repository-name=$2 -tag=$3 -access-key-id=$4 -secret-access-key=$5 )
+image_exists=$(go run ecr.go -region="$1" -repository-name="$2" -tag="$3" -access-key-id="$4" -secret-access-key="$5")
 echo "::set-output name=image-exists::$image_exists"
